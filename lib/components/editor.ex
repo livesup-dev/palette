e969d67eebaee32d3d07ce82a0c982e0e17a95c7
mustela <ejankowski@gmail.com>
2023-02-01 01:07:48 +0100

    fixing editor

--- lib/components/editor.ex
+++ lib/components/editor.ex
@@ -6,9 +6,10 @@ defmodule Palette.Components.Editor do
   attr(:value, :string, required: true)
   attr(:mode, :atom, default: :html, values: [:html])
 
+  @spec editor(map) :: Phoenix.LiveView.Rendered.t()
   def editor(assigns) do
     ~H"""
-    <div id="editor" class="editor-parent" phx-hook="editor" data-mode={@mode}>
+    <div id="editor" class="editor-parent" phx-update="ignore" phx-hook="editor" data-mode={@mode}>
       <div id="ace-editor" class="editor"><%= @value %></div>
     </div>
     <Input.hidden_input name="editor-content" value={@value} />
