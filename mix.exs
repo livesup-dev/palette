3b2307cbf0c974ea7bb14ed526a3a0b1558fd362
mustela <ejankowski@gmail.com>
2023-02-17 17:34:44 +0100

    bumping v

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Palette.MixProject do
   def project do
     [
       app: :palette,
-      version: "0.1.26",
+      version: "0.1.27",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
