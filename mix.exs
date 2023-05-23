33486a1f8a147153207790c7604328efc2d3dbf8
mustela <ejankowski@gmail.com>
2023-05-23 14:15:19 +0200

    Updating liveview

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Palette.MixProject do
   def project do
     [
       app: :palette,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -28,7 +28,9 @@ defmodule Palette.MixProject do
       # Phoenix
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.4", only: :dev},
-      {:phoenix_live_view, "~> 0.18"},
+      # {:phoenix_live_view, "~> 0.18"},
+      {:phoenix_live_view,
+       github: "phoenixframework/phoenix_live_view", branch: "main", override: true},
       {:jason, "~> 1.0"},
       {:timex, ">= 0.0.0"},
       {:excoveralls, "~> 0.15", only: :test},
