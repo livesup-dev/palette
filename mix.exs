b23cadaf53f90137dbee326d120da1dda7790076
mustela <ejankowski@gmail.com>
2023-09-04 16:07:56 +0200

    updating css

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Palette.MixProject do
   def project do
     [
       app: :palette,
-      version: "0.2.7",
+      version: "0.2.8",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
